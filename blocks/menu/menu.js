bcd9b6aeb482c4998f2bebf0d3dc2ef6ef9c65e0
Mikhail Varushichev <Mikeloangel@gmail.com>
2022-10-05 15:24:03 +0300

    adds burger menu

---
+++ blocks/menu/menu.js
@@ -0,0 +1,46 @@
+export default class Menu{
+  #menuElement;
+  #burgerElement;
+  #menuContainerElement;
+  #closeElement;
+
+  constructor({selector = 'menu'}){
+    this.#menuElement = document.querySelector(`.${selector}`);
+    this.#burgerElement = this.#menuElement.querySelector('.menu__burger');
+    this.#menuContainerElement = this.#menuElement.querySelector('.menu__backdrop');
+    this.#closeElement = this.#menuElement.querySelector('.menu__close');
+
+    this.handleEscClose = this.handleEscClose.bind(this);
+
+    this.#setListeners();
+
+    this.open();
+  }
+
+  #setListeners(){
+    this.#burgerElement.addEventListener('click',this.open.bind(this));
+    this.#closeElement.addEventListener('click', this.close.bind(this))
+  }
+
+  handleEscClose(e){
+    if(e.key==='Escape'){
+      this.close();
+    }
+  }
+
+  open(){
+    this.#menuContainerElement.classList.add('menu__container_visible');
+    this.#burgerElement.classList.add('menu__burger_active');
+    this.#closeElement.classList.remove('.menu__close_hidden');
+
+    window.addEventListener('keydown',this.handleEscClose);
+  }
+
+  close(){
+    this.#menuContainerElement.classList.remove('menu__container_visible');
+    this.#burgerElement.classList.remove('menu__burger_active');
+    this.#closeElement.classList.add('.menu__close_hidden');
+
+    window.removeEventListener('keydown',this.handleEscClose);
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-import SearchBox from "../blocks/search-box/search-box.js"
-import enableLangSelect from "../blocks/lang/lang.js"
+import SearchBox from "../blocks/search-box/search-box.js";
+import enableLangSelect from "../blocks/lang/lang.js";
+import Menu from "../blocks/menu/menu.js";
 
 const search = new SearchBox({boxSelector: 'search-box'});
 enableLangSelect();
+const menu = new Menu({});
