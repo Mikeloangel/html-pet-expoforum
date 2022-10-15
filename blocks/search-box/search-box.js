dbc9d74edf8aa37b6a463b61b0e7d130f690509f
Mikhail Varushichev <Mikeloangel@gmail.com>
2022-10-15 12:56:48 +0300

    fix: updates search box component

--- blocks/search-box/search-box.js
+++ blocks/search-box/search-box.js
@@ -1,32 +1,45 @@
 export default class SearchBox {
-  #searchElement;
+  #searchFormElement;
   #inputElement;
-  #buttonElement;
-  #isOpen = false;
+  #onSubmitCallBack;
 
-  constructor({ boxSelector }) {
-    this.#searchElement = document.querySelector(`.${boxSelector}`);
-    this.#inputElement = this.#searchElement.querySelector('.search-box__input');
-    this.#buttonElement = this.#searchElement.querySelector('.search-box__button');
+  constructor({ idSelector = "searchbox", onSubmit = null }) {
+    this.#searchFormElement = document.querySelector(`#${idSelector}`);
+    this.#inputElement = this.#searchFormElement.querySelector('.search-box__input');
+
+    this.#onSubmitCallBack = onSubmit;
 
     this.#setEventListeners();
   }
 
   #setEventListeners() {
-    this.#buttonElement.addEventListener('click', () => {
-      if (this.#isOpen) {
-        if (this.#inputElement.value.length !== 0) {
-          window.open(`https://yandex.ru/search/?text=${this.#inputElement.value}`);
-          this.#inputElement.value = '';
-        }
-        this.#isOpen = false;
-      } else {
-        this.#inputElement.focus();
-        this.#isOpen = true;
-      }
-    });
-
+    this.#searchFormElement.addEventListener('submit', this.#handleSubmit);
+    this.#inputElement.addEventListener('blur', () => { setTimeout(this.#handleClose, 100) });
   }
 
+  #handleOpen = () => {
+    this.#inputElement.classList.add('search-box__input_visible');
+    setTimeout(() => this.#inputElement.focus(), 100);
+  }
+
+  #handleClose = () => {
+    this.#inputElement.classList.remove('search-box__input_visible');
+  }
+
+  #handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.#inputElement.classList.contains('search-box__input_visible') && this.#inputElement.value.length > 0) {
+      if (typeof this.#onSubmitCallBack === 'function') {
+        this.#onSubmitCallBack(this.#inputElement.value);
+      } else {
+        console.log(`SEARCHING FOR: ${this.#inputElement.value}`);
+      }
+
+      this.#searchFormElement.reset();
+      this.#handleClose();
+    } else {
+      this.#handleOpen();
+    }
+  }
 }
 
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,10 @@ import SearchBox from "../blocks/search-box/search-box.js";
 import enableLangSelect from "../blocks/lang/lang.js";
 import Menu from "../blocks/menu/menu.js";
 
-const search = new SearchBox({boxSelector: 'search-box'});
+const search = new SearchBox({
+  onSubmit: (query) => {
+    window.open(`https://www.google.com/search?q=${query}`);
+  }
+});
 enableLangSelect();
 const menu = new Menu({});
